ab89af49c3594b26519a0620d76aa4abf47f74b5
Ryan Kim <rkim@pros.com>
2014-05-28 15:27:13 -0500

    Removed unnecessary save call.

--- app/scripts/controllers/UserController.js
+++ app/scripts/controllers/UserController.js
@@ -1,8 +1,6 @@
 EmberApp.UserController = Ember.Controller.extend({
-    needs: 'phones',
     actions: {
         save: function(user) {
-            this.get('controllers.phones.model').save();
             user.save();
         }
     }
